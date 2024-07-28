6515a0fab29c8df9979f639149512784d3c518b1
Quenton Hua <hua.quenton@gmail.com>
2024-07-28 15:52:49 -0500

    about me section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
 import logo from './logo.svg';
+import { LinkOut, OctocatFill } from 'akar-icons';
 import './App.css';
+
+function ProjectCard() {
+  return (
+    <div className="project-card">
+      <div className="thumbnail">
+        <div className="frosted-glass">
+          <div className="buttons-section">
+            <button>
+              <LinkOut strokeWidth={2} size={36} />
+            </button>
+            <button>
+              <OctocatFill strokeWidth={2} size={36} />
+            </button>
+
+
+          </div>
+
+        </div>
+      </div>
+      <div className="content">
+        <h1>uDrink</h1>
+        <h5 className="skills-section"> React Native, Firebase</h5>
+        <div className="divider"></div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maximus est id sem accumsan, sit amet laoreet turpis feugiat. Sed a velit quis justo tristique mollis a a nisl. Nunc sed pharetra odio.
+        </p>
+        <div className="gap-filler"></div>
+        <h5 className="time-period">Feb - May 2024</h5>
+      </div>
+    </div>
+  );
+}
+
+
+function IntroductionSection() {
+  return (
+    <section className="introduction">
+      <div className="title">
+        <div className="row1"> <h1>QUENTON</h1></div>
+        <div className="row2"> <h1>HUA</h1> <div className="subtitle-container">
+          <div>
+            <h2> <b className="role">Full-Stack Developer</b>, </h2>
+            <h2> Computer Engineer @ <b className="school">TAMU</b> </h2>
+            <div className="button-tray"><div className="icon-tray"> <button> Test1</button> <button> Test2</button></div>    </div>
+          </div></div>
+        </div>
+      </div>
+    </section>
+
+  )
+}
+
+function ProjectSection() {
+  return (
+    <section className="projects">
+      <ProjectCard />
+    </section>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <ProjectSection /> */}
+      <IntroductionSection />
     </div>
   );
 }
